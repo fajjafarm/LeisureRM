95052b6f19981b92fbb63a62cd483eacd66ad828
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-11-17 15:27:56 +0000

    1

--- database/migrations/2025_00_00_000002_01_create_businesses_table.php
+++ database/migrations/2025_00_00_000002_01_create_businesses_table.php
@@ -1,13 +1,10 @@
 <?php
 
-// File: database/migrations/0000_00_00_000001_create_businesses_table.php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('businesses', function (Blueprint $table) {
@@ -17,9 +14,5 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
-    public function down(): void
-    {
-        Schema::dropIfExists('businesses');
-    }
+    public function down(): void { Schema::dropIfExists('businesses'); }
 };
