09646705e1f12d0d0f94d9370f95ba8b0bf2d921
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-11-17 08:58:39 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,10 @@ Route ::middleware(['auth', 'verified'])->group(function () {
 });
 
 // If you already have a routes/web.php with lots of routes, just make sure the above is at the top!
+Route::group(['prefix' => '/', 'middleware' => 'auth'], function () {
+    Route::get('', [RoutingController::class, 'index'])->name('root');
+    Route::get('/home', fn()=>view('dashboards.index'))->name('home');
+    Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
+    Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
+    Route::get('{any}', [RoutingController::class, 'root'])->name('any');
+});
